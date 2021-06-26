843a9212cf889a0749f02e91251a47e6b56efac6
Bhavana Kumari <62162708+Bhavanak17@users.noreply.github.com>
2021-06-26 15:03:49 +0530

    Delete keys.js

--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    MONGOURI:"mongodb+srv://idyllic:idylliciswinner@idyllic.ihahy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"tuntunmausichutkikimummaplusladdoo"
-    }
