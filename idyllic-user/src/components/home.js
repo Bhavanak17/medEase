573dc830c75bff18bef8e4ea954807680eb28b65
Bhavana Kumari <62162708+Bhavanak17@users.noreply.github.com>
2021-06-27 09:46:20 +0530

    Add files via upload

--- idyllic-user/src/components/home.js
+++ idyllic-user/src/components/home.js
@@ -5,15 +5,13 @@ const Home = () => {
         <div className="home-div">
             <div className="banner">
             <div className="content">
-        <h1>MEDEASE</h1>
+        <h1>medEase</h1>
         <h2>Close to Home. Close to You. ❤</h2><br/><br/>
-        <p>MEDEASE mean "medicines avaialble easily". We are around you, with you to help you. <br/>We will make sure that you are aware of evey medicine you need whether it is available around you or not</p>
+        <p>medEase mean "medicines avaialble easily". We are around you, with you to help you. <br/>We will make sure that you are aware of evey medicine you need whether it is available around you or not</p>
         
         </div><br/>
         <div className="project-div">
-            <p>PROBLEM STATEMENT: The main problem everyone facing today in covid-19  situations is<br/> 
-            mainly non availability of medicines at the moment and efforts for searching it around when movement is abided .<br/>
-            Our application resolves this problem by making direct contact with the shopkeepers<br/> 
+        <p>medEase application resolves this problem by making direct contact with the shopkeepers<br/> 
             around us and getting updated by them about the availabilty of the medicines</p>
         </div>
         </div>
